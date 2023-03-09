18c0deb8eac3006373a9298e697ea127ac8d5585
David Benner <davidbenner@davids-mbp.lan>
2023-03-08 20:49:48 -0500

    adds imageSchema, and image route

---
+++ models/image.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const imageSchema = new mongoose.Schema({
+  image: {
+    type: String,
+  },
+});
+
+const Image = new mongoose.model("Image", imageSchema);
+module.exports;
